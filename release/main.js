615fb2e8f54339fb99c3217b80e41090f7c40279
Anton <anton.bilousov@gmail.com>
2017-06-03 14:35:27 +0300

    gitignore

--- release/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var app_module_1 = require('./app.module');
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
